1b414e216084d926e0b7970a503fb57306efd3f1
zmajcek <srdjan.bozovic@live.com>
2013-10-21 00:12:44 +0200

    http client service

---
+++ src/MsCampus.Win8.Shared/Implementation/Services/HttpClientService.cs
@@ -0,0 +1,25 @@
+﻿using MsCampus.Win8.Shared.Contracts.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsCampus.Win8.Shared.Implementation.Services
+{
+    public class HttpClientService : IHttpClientService
+    {
+        public async Task<T> GetJson<T>(string url)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            var client = new HttpClient(httpClientHandler);
+            var response = await client.GetAsync(url);
+            if (response != null)
+                return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+            else
+                return default(T);
+        }
+    }
+}
